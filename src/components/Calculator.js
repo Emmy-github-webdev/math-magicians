c582c1aae0f0ceec3e6c64c86161e0abf7f1be56
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-11-30 19:52:00 +0100

    Add calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -11,35 +11,35 @@ class Calculator extends React.Component {
   render() {
     return (
       <div className="calc-content">
+        <div className="calc-input">0</div>
         <div className="calc-row">
           <button className="calc-button" type="button">AC</button>
           <button className="calc-button" type="button">+/-</button>
           <button className="calc-button" type="button">%</button>
-          <button className="calc-button calc-operator" type="button">÷</button>
+          <button className="calc-operator" type="button">÷</button>
         </div>
         <div className="calc-row">
           <button className="calc-button" type="button">7</button>
           <button className="calc-button" type="button">8</button>
           <button className="calc-button" type="button">9</button>
-          <button className="calc-button calc-operator" type="button">X</button>
+          <button className="calc-operator" type="button">X</button>
         </div>
         <div className="calc-row">
           <button className="calc-button" type="button">4</button>
           <button className="calc-button" type="button">5</button>
           <button className="calc-button" type="button">6</button>
-          <button className="calc-button calc-operator" type="button">-</button>
+          <button className="calc-operator" type="button">-</button>
         </div>
         <div className="calc-row">
           <button className="calc-button" type="button">1</button>
           <button className="calc-button" type="button">2</button>
           <button className="calc-button" type="button">3</button>
-          <button className="calc-button calc-operator" type="button">+</button>
+          <button className="calc-operator" type="button">+</button>
         </div>
         <div className="calc-row">
           <button className="calc-zero" type="button">0</button>
-          {/* <button className="calc-button" type="button">+/-</button> */}
           <button className="calc-button" type="button">.</button>
-          <button className="calc-button calc-operator" type="button">=</button>
+          <button className="calc-operator" type="button">=</button>
         </div>
       </div>
     );
