adbcd5128de4bebb9ea3cfb39cf9f2b660ce0c88
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-12-01 19:27:28 +0100

    Add click event to calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,45 +3,63 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import './Calculator.css';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      next: 0,
+    };
+
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  clickHandler(e) {
+    this.setState((previousState) => calculate(previousState, e.target.innerText));
   }
 
   render() {
+    let val = 0;
+    const { total, next } = this.state;
+    if (next) {
+      val = next;
+    } else if (total) {
+      val = total;
+    }
+
     return (
       <div className="calc-content">
-        <div className="calc-input">0</div>
+        <div className="calc-input">{val}</div>
         <div className="calc-row">
-          <button className="calc-button" type="button">AC</button>
-          <button className="calc-button" type="button">+/-</button>
-          <button className="calc-button" type="button">%</button>
-          <button className="calc-operator" type="button">÷</button>
+          <button className="calc-button" type="button" onClick={this.clickHandler}>AC</button>
+          <button className="calc-button" type="button" onClick={this.clickHandler}>+/-</button>
+          <button className="calc-button" type="button" onClick={this.clickHandler}>%</button>
+          <button className="calc-operator" type="button" onClick={this.clickHandler}>÷</button>
         </div>
         <div className="calc-row">
-          <button className="calc-button" type="button">7</button>
-          <button className="calc-button" type="button">8</button>
-          <button className="calc-button" type="button">9</button>
-          <button className="calc-operator" type="button">X</button>
+          <button className="calc-button" type="button" onClick={this.clickHandler}>7</button>
+          <button className="calc-button" type="button" onClick={this.clickHandler}>8</button>
+          <button className="calc-button" type="button" onClick={this.clickHandler}>9</button>
+          <button className="calc-operator" type="button" onClick={this.clickHandler}>X</button>
         </div>
         <div className="calc-row">
-          <button className="calc-button" type="button">4</button>
-          <button className="calc-button" type="button">5</button>
-          <button className="calc-button" type="button">6</button>
-          <button className="calc-operator" type="button">-</button>
+          <button className="calc-button" type="button" onClick={this.clickHandler}>4</button>
+          <button className="calc-button" type="button" onClick={this.clickHandler}>5</button>
+          <button className="calc-button" type="button" onClick={this.clickHandler}>6</button>
+          <button className="calc-operator" type="button" onClick={this.clickHandler}>-</button>
         </div>
         <div className="calc-row">
-          <button className="calc-button" type="button">1</button>
-          <button className="calc-button" type="button">2</button>
-          <button className="calc-button" type="button">3</button>
-          <button className="calc-operator" type="button">+</button>
+          <button className="calc-button" type="button" onClick={this.clickHandler}>1</button>
+          <button className="calc-button" type="button" onClick={this.clickHandler}>2</button>
+          <button className="calc-button" type="button" onClick={this.clickHandler}>3</button>
+          <button className="calc-operator" type="button" onClick={this.clickHandler}>+</button>
         </div>
         <div className="calc-row">
-          <button className="calc-zero" type="button">0</button>
-          <button className="calc-button" type="button">.</button>
-          <button className="calc-operator" type="button">=</button>
+          <button className="calc-zero" type="button" onClick={this.clickHandler}>0</button>
+          <button className="calc-button" type="button" onClick={this.clickHandler}>.</button>
+          <button className="calc-operator" type="button" onClick={this.clickHandler}>=</button>
         </div>
       </div>
     );
