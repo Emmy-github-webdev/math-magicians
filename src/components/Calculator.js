49c6bf6196b104ba42704e14d2564de79e5d8ddc
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-12-02 20:57:43 +0100

    Implement review recommendation

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,3 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable indent */
-/* eslint-disable react/prefer-stateless-function */
 import React, { useState } from 'react';
 import './Calculator.css';
 import calculate from '../logic/calculate';
@@ -28,40 +25,40 @@ const Calculator = () => {
     result(ButtonName);
   };
 
-    return (
-      <div className="calc-content">
-        <div className="calc-input">{val}</div>
-        <div className="calc-row">
-          <button className="calc-button" type="button" onClick={clickHandler}>AC</button>
-          <button className="calc-button" type="button" onClick={clickHandler}>+/-</button>
-          <button className="calc-button" type="button" onClick={clickHandler}>%</button>
-          <button className="calc-operator" type="button" onClick={clickHandler}>÷</button>
-        </div>
-        <div className="calc-row">
-          <button className="calc-button" type="button" onClick={clickHandler}>7</button>
-          <button className="calc-button" type="button" onClick={clickHandler}>8</button>
-          <button className="calc-button" type="button" onClick={clickHandler}>9</button>
-          <button className="calc-operator" type="button" onClick={clickHandler}>x</button>
-        </div>
-        <div className="calc-row">
-          <button className="calc-button" type="button" onClick={clickHandler}>4</button>
-          <button className="calc-button" type="button" onClick={clickHandler}>5</button>
-          <button className="calc-button" type="button" onClick={clickHandler}>6</button>
-          <button className="calc-operator" type="button" onClick={clickHandler}>-</button>
-        </div>
-        <div className="calc-row">
-          <button className="calc-button" type="button" onClick={clickHandler}>1</button>
-          <button className="calc-button" type="button" onClick={clickHandler}>2</button>
-          <button className="calc-button" type="button" onClick={clickHandler}>3</button>
-          <button className="calc-operator" type="button" onClick={clickHandler}>+</button>
-        </div>
-        <div className="calc-row">
-          <button className="calc-zero" type="button" onClick={clickHandler}>0</button>
-          <button className="calc-button" type="button" onClick={clickHandler}>.</button>
-          <button className="calc-operator" type="button" onClick={clickHandler}>=</button>
-        </div>
+  return (
+    <div className="calc-content">
+      <div className="calc-input">{val}</div>
+      <div className="calc-row">
+        <button className="calc-button" type="button" onClick={clickHandler}>AC</button>
+        <button className="calc-button" type="button" onClick={clickHandler}>+/-</button>
+        <button className="calc-button" type="button" onClick={clickHandler}>%</button>
+        <button className="calc-operator" type="button" onClick={clickHandler}>÷</button>
       </div>
-    );
+      <div className="calc-row">
+        <button className="calc-button" type="button" onClick={clickHandler}>7</button>
+        <button className="calc-button" type="button" onClick={clickHandler}>8</button>
+        <button className="calc-button" type="button" onClick={clickHandler}>9</button>
+        <button className="calc-operator" type="button" onClick={clickHandler}>x</button>
+      </div>
+      <div className="calc-row">
+        <button className="calc-button" type="button" onClick={clickHandler}>4</button>
+        <button className="calc-button" type="button" onClick={clickHandler}>5</button>
+        <button className="calc-button" type="button" onClick={clickHandler}>6</button>
+        <button className="calc-operator" type="button" onClick={clickHandler}>-</button>
+      </div>
+      <div className="calc-row">
+        <button className="calc-button" type="button" onClick={clickHandler}>1</button>
+        <button className="calc-button" type="button" onClick={clickHandler}>2</button>
+        <button className="calc-button" type="button" onClick={clickHandler}>3</button>
+        <button className="calc-operator" type="button" onClick={clickHandler}>+</button>
+      </div>
+      <div className="calc-row">
+        <button className="calc-zero" type="button" onClick={clickHandler}>0</button>
+        <button className="calc-button" type="button" onClick={clickHandler}>.</button>
+        <button className="calc-operator" type="button" onClick={clickHandler}>=</button>
+      </div>
+    </div>
+  );
 };
 
 export default Calculator;
