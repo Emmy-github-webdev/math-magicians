0b67be82c5f4309c8e2afb8dd0eadc6f9249d404
Emmanuel Ogah <68148157+Emmy-github-webdev@users.noreply.github.com>
2021-12-09 00:02:48 +0100

    update header test

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-<<<<<<< HEAD
-import {Link} from '../Link.react';
-=======
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './Header';
->>>>>>> 368a45ac4074e87e73098b85a7a9c62cb22b5827
 
 
 test('renders correctly', () => {
