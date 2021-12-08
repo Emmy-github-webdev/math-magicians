bb37f4ae01e23ac76ec4ff1b3263d354a121d717
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-12-08 20:41:21 +0100

    Test for calculate and operate passed

---
+++ src/logic/calculate.test.js
@@ -0,0 +1,6 @@
+import React from "react";
+import calculate from "./calculate";
+
+it('Calculte should work without error', () => {
+  expect(calculate.length).toBe(2);
+})
---
+++ src/logic/operate.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import operate from './operate';
+
+it('Operate should work without error', () => {
+  expect(operate.length).toBe(3);
+})
