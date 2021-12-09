cc7c8963f59533db757934a6a1832f37c3ffdb1a
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-12-09 09:34:30 +0100

    Import react testing library

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import '@testing-library/jest-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './Header';
 
-test('renders correctly', () => {
+it('renders correctly', () => {
   const Nav = renderer
     .create(<Router><Header /></Router>)
     .toJSON();
